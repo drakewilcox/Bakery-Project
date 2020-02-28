db133cba0a99fdc146ca8f6f0116802007bfc7f4
Drake Wilcox <drakewilcox@gmail.com>
2020-02-28 10:37:20 -0800

    add boilerplate for CustomerOrder namespace

---
+++ Program.cs
@@ -0,0 +1,16 @@
+using System; 
+using System.Collections.Generic; 
+// using Bakery.Models; 
+
+namespace CustomerOrder
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Welcome to Pierre's Bakery! [Press ENTER to Continue]");
+      Console.ReadLine();
+      Console.WriteLine("User Pressed Enter!");
+    }
+  }
+}
